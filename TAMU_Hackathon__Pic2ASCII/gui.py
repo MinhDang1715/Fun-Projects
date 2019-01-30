a86d996ac363a02f691ff5d018f1be87cc9c90c8
Minh Dang <35425810+MinhDang1715@users.noreply.github.com>
2019-01-29 22:16:52 -0600

    Add files via upload

--- TAMU_Hackathon__Pic2ASCII/gui.py
+++ TAMU_Hackathon__Pic2ASCII/gui.py
@@ -2,8 +2,8 @@ import sys
 import os
 import main
 
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PIL import Image
 
 input_file = ''
 
@@ -18,6 +18,7 @@ class App(QWidget):
         self.height = 300
         self.window = QWidget()
         self.layout = QVBoxLayout()
+        self.setWindowIcon(QIcon("C:/Users/Minh Dang/Desktop/Pic_to_ASCII/icon.png"))
         self.initUI()
 
     def initUI(self):
@@ -53,8 +54,7 @@ class App(QWidget):
         if input_file:
             file_type = input_file[len(input_file) - 3: len(input_file)]
             if file_type == 'jpg' or file_type == 'png':
-                dialog = QFileDialog.getSaveFileName(self, 'Text Files',
-                                                     './', 'Text Files(*.txt)')
+                dialog = QFileDialog.getSaveFileName(self, 'Text Files', './', 'Text Files(*.txt)')
                 if dialog:
                     output_file = dialog[0]
                     if main.convertImage(input_file, output_file):
@@ -65,35 +65,72 @@ class App(QWidget):
                         msgBox.buttonClicked.connect(msg_box_click)
                         msgBox.exec_()
             else:
-                dialog = QFileDialog.getSaveFileName(self, 'Gif',
-                                                     './', 'Gif(*.gif)')
-                if dialog:
-                    output_file = dialog[0]
-                    # get each frame
-                    for i, frame in enumerate(main.getFrame(input_file)):
-                        frame.save('gif_frame/frame%d.png' % i, **frame.info)
+                # choose between detail or simple mode
+                menu_mode = QMessageBox()
+                menu_mode.setWindowTitle('Choose Mode')
+                menu_mode.setText("Please select the desire mode for the converted gif")
+                menu_mode.setGeometry(700, 500, 800, 200)
+                detail_mode = QPushButton('Detail Mode')
+                simple_mode = QPushButton('Simple Mode')
+                menu_mode.addButton(detail_mode, QMessageBox.YesRole)
+                menu_mode.addButton(simple_mode, QMessageBox.NoRole)
+                ret = menu_mode.exec_()
 
-                    # convert all the frame to a .txt file
-                    DIR = 'gif_frame/'
-                    DIR_2 = 'gif_frame_text/'
-                    list_file = os.listdir(DIR)
-                    for i in range(len(list_file)):
-                        main.convertGif(DIR + list_file[i], DIR_2 + 'frame(%d).txt' % i)
+                dialog = QFileDialog.getSaveFileName(self, 'Gif', './', 'Gif(*.gif)')
 
-                    # process the .txt files
-                    DIR_3 = 'to_image/'
-                    list_file = os.listdir(DIR_2)
-                    for i in range(len(list_file)):
-                        main.textToImage(DIR_2 + list_file[i], DIR_3 + 'frame(%d).png' % i)
+                # notice that press detail_mode will return 0
+                # and simple mode will return 1
+                if not ret:
+                    if dialog:
+                        output_file = dialog[0]
+                        # get each frame
+                        for i, frame in enumerate(main.getFrame(input_file)):
+                            frame.save('gif_frame/frame%d.png' % i, **frame.info)
 
-                    DIR_3 = 'to_image/'
-                    # convert all the .process png to .gif
-                    if main.imagesToGif(DIR_3, output_file):
-                        msgBox = QMessageBox()
-                        msgBox.setIcon(QMessageBox.Information)
-                        msgBox.setText("Click OK to open the file.")
-                        msgBox.setWindowTitle("Convert successful")
-                        msgBox.exec_()
+                        # convert all the frame to a .txt file
+                        DIR = 'gif_frame/'
+                        DIR_2 = 'gif_frame_text/'
+                        list_file = os.listdir(DIR)
+                        for i in range(len(list_file)):
+                            main.convertGifDetail(DIR + list_file[i], DIR_2 + 'frame(%d).txt' % i)
+
+                        # process the .txt files
+                        DIR_3 = 'to_image/'
+                        list_file = os.listdir(DIR_2)
+                        for i in range(len(list_file)):
+                            main.textToImageDetail(DIR_2 + list_file[i], DIR_3 + 'frame(%d).png' % i)
+                    else:
+                        pass
+                else:
+                    if dialog:
+                        output_file = dialog[0]
+                        # get each frame
+                        for i, frame in enumerate(main.getFrame(input_file)):
+                            frame.save('gif_frame/frame%d.png' % i, **frame.info)
+
+                        # convert all the frame to a .txt file
+                        DIR = 'gif_frame/'
+                        DIR_2 = 'gif_frame_text/'
+                        list_file = os.listdir(DIR)
+                        for i in range(len(list_file)):
+                            main.convertGifSimple(DIR + list_file[i], DIR_2 + 'frame(%d).txt' % i)
+
+                        # process the .txt files
+                        DIR_3 = 'to_image/'
+                        list_file = os.listdir(DIR_2)
+                        for i in range(len(list_file)):
+                            main.textToImageSimple(DIR_2 + list_file[i], DIR_3 + 'frame(%d).png' % i)
+                    else:
+                        pass
+
+                DIR_3 = 'to_image/'
+                # convert all the .process png to .gif
+                if main.imagesToGif(DIR_3, output_file):
+                    msgBox = QMessageBox()
+                    msgBox.setIcon(QMessageBox.Information)
+                    msgBox.setText("Convert successful.")
+                    msgBox.setWindowTitle("Done")
+                    msgBox.exec_()
 
 
 if __name__ == '__main__':
--- TAMU_Hackathon__Pic2ASCII/main.py
+++ TAMU_Hackathon__Pic2ASCII/main.py
@@ -6,9 +6,9 @@ from PIL import Image, ImageDraw, ImageFont
 from math import trunc
 
 def convertImage(in_file, out_file):
-    # default size 1000x500
+    # default size 1000x284
     base_width = 1000
-    base_size = 500
+    base_size = 284
     # open the image and grey scale it
     img = Image.open(in_file).convert('L')
     # resize the image
@@ -45,11 +45,16 @@ def getFrame(in_file):
     except EOFError:
         pass
 
+# note for ratio
+# the width of the output gif will be the base_width of the frame * 2
+# the size(height) of the output gif will be the same as the base_size of the frame
+# however be careful with the size of the text and the vertical + horizontal scale
 
-def convertGif(in_file, out_file):
+# detail mode
+def convertGifDetail(in_file, out_file):
     # default size 256x256
-    base_width = 256
-    base_size = 256
+    base_width = 250
+    base_size = 500
     # open the image and grey scale it
     img = Image.open(in_file).convert('L')
     # resize the image
@@ -69,11 +74,11 @@ def convertGif(in_file, out_file):
     return True
 
 
-def textToImage(input_file, output_file):
+def textToImageDetail(input_file, output_file):
     font = ImageFont.truetype('cour.ttf', 3)
-    img = Image.new('L', (500, 256), color=255)
+    img = Image.new('L', (500, 500), color=255)
     draw = ImageDraw.Draw(img)
-    vertical_scale = 5
+    vertical_scale = 0
     with open(input_file) as f:
         lines = f.readlines()
 
@@ -83,10 +88,63 @@ def textToImage(input_file, output_file):
     img.save(output_file)
 
 
+# simple mode
+def convertGifSimple(in_file, out_file):
+    # default size 100x80
+    base_width = 50
+    base_size = 30
+    # open the image and grey scale it
+    img = Image.open(in_file).convert('L')
+    # resize the image
+    img = img.resize((base_width, base_size), Image.ANTIALIAS)
+    # get the pixel array
+    pixel_Arr = np.array(img)
+    # ASCII art
+    ASCII = ['.', ',', '*', '^', '!', '|', '+', '-', ':', ';', 'o', 'x', '%', '?', '$', '#', '@']
+    # assign each character to it corresponding pixel value
+    f = open(out_file, 'w')
+    for x in pixel_Arr:
+        for y in x:
+            f.write(ASCII[trunc(y/255*17) - 1])
+        f.write('\n')
+    # close file
+    f.close()
+    return True
+
+
+def textToImageSimple(input_file, output_file):
+    font = ImageFont.truetype('cour.ttf', 10)
+    # img = Image.new('L', (800, 800), color=255)
+    img = Image.new('L', (300, 300), color=255)
+    draw = ImageDraw.Draw(img)
+    vertical_scale = 0
+    horizontal_scale = 0
+    with open(input_file) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        draw.text((horizontal_scale, vertical_scale), line, fill=0, font=font)
+        vertical_scale = vertical_scale + 10
+    img.save(output_file)
+
+
 def imagesToGif(input_dir, output_file):
     images = []
     list_file = os.listdir(input_dir)
     for filename in list_file:
         images.append(imageio.imread(input_dir + filename))
     imageio.mimsave(output_file, images)
+    removeFiles()
     return True
+
+
+def removeFiles():
+    list_file = os.listdir('gif_frame/')
+    for file in list_file:
+        os.remove('gif_frame/' + file)
+    list_file = os.listdir('gif_frame_text/')
+    for file in list_file:
+        os.remove('gif_frame_text/' + file)
+    list_file = os.listdir('to_image/')
+    for file in list_file:
+        os.remove('to_image/' + file)
