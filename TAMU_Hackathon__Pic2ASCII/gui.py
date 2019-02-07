bb6ef22aa6f47c267c11278b60293d4e3019bc8c
Minh Dang <35425810+MinhDang1715@users.noreply.github.com>
2019-02-06 18:15:52 -0600

    Add files via upload

--- TAMU_Hackathon__Pic2ASCII/gui.py
+++ TAMU_Hackathon__Pic2ASCII/gui.py
@@ -2,7 +2,7 @@ import sys
 import os
 import main
 
-from PyQt5.QtGui import *
+from natsort import natsorted
 from PyQt5.QtWidgets import *
 
 input_file = ''
@@ -18,32 +18,38 @@ class App(QWidget):
         self.height = 300
         self.window = QWidget()
         self.layout = QVBoxLayout()
-        self.setWindowIcon(QIcon("C:/Users/Minh Dang/Desktop/Pic_to_ASCII/icon.png"))
         self.initUI()
 
     def initUI(self):
         self.window.setWindowTitle(self.title)
         self.window.setGeometry(self.left, self.top, self.width, self.height)
 
-        btn_import = QPushButton('Import Picture', self)
+        btn_import = QPushButton('Import', self)
         btn_import.clicked.connect(self.btn_import_click)
         btn_import.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
 
-        btn_export = QPushButton('Export Picture', self)
+        btn_export = QPushButton('Export', self)
         btn_export.clicked.connect(self.btn_export_click)
         btn_export.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
 
+        btn_cam = QPushButton('Open Webcam', self)
+        # btn_cam.clicked.connect(self.btn_export_click)
+        btn_cam.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+
         self.layout.addWidget(btn_import)
         self.layout.addWidget(btn_export)
+        self.layout.addWidget(btn_cam)
         self.window.setLayout(self.layout)
         self.window.show()
 
     def btn_import_click(self):
+        # get the input file
         global input_file
-        dialog = QFileDialog.getOpenFileName(self, 'Picture Files',
-                                             './', 'Image Files(*.png *.jpg *.gif)')
+        dialog = QFileDialog.getOpenFileName(self, 'Import Files', './', 'Image Files(*.png *.jpg *.gif)')
         if dialog:
             input_file = dialog[0]
+        else:
+            pass
 
     def btn_export_click(self):
         def msg_box_click():
@@ -54,16 +60,47 @@ class App(QWidget):
         if input_file:
             file_type = input_file[len(input_file) - 3: len(input_file)]
             if file_type == 'jpg' or file_type == 'png':
-                dialog = QFileDialog.getSaveFileName(self, 'Text Files', './', 'Text Files(*.txt)')
-                if dialog:
-                    output_file = dialog[0]
-                    if main.convertImage(input_file, output_file):
-                        msgBox = QMessageBox()
-                        msgBox.setIcon(QMessageBox.Information)
-                        msgBox.setText("Click OK to open the file.")
-                        msgBox.setWindowTitle("Convert successful")
-                        msgBox.buttonClicked.connect(msg_box_click)
-                        msgBox.exec_()
+                # choose the output of the picture
+                menu_mode = QMessageBox()
+                menu_mode.setWindowTitle('Choose Mode')
+                menu_mode.setText("Please select the output of the picture")
+                menu_mode.setGeometry(700, 500, 800, 200)
+                text_mode = QPushButton('Text')
+                pic_mode = QPushButton('Picture')
+                menu_mode.addButton(text_mode, QMessageBox.YesRole)
+                menu_mode.addButton(pic_mode, QMessageBox.NoRole)
+                ret = menu_mode.exec_()
+
+                # get the output file
+                if not ret:
+                    dialog = QFileDialog.getSaveFileName(self, 'Export Text File', './', 'Text Files(*.txt)')
+                    try:
+                        if dialog:
+                            output_file = dialog[0]
+                            # txt mode
+                            if main.convertImage(input_file, output_file):
+                                msgBox = QMessageBox()
+                                msgBox.setIcon(QMessageBox.Information)
+                                msgBox.setText("Click OK to open the file.")
+                                msgBox.setWindowTitle("Convert successful")
+                                msgBox.buttonClicked.connect(msg_box_click)
+                                msgBox.exec_()
+                    except IOError as io_error:
+                        pass
+                else:
+                    dialog = QFileDialog.getSaveFileName(self, 'Export Picture', './', 'Picture(*.png)')
+                    try:
+                        if dialog:
+                            output_file = dialog[0]
+                            if main.picToImage(input_file, output_file):
+                                msgBox = QMessageBox()
+                                msgBox.setIcon(QMessageBox.Information)
+                                msgBox.setText("Convert successful.")
+                                msgBox.setWindowTitle("Done")
+                                msgBox.exec_()
+                    except IOError as io_error:
+                        pass
+
             else:
                 # choose between detail or simple mode
                 menu_mode = QMessageBox()
@@ -75,62 +112,58 @@ class App(QWidget):
                 menu_mode.addButton(detail_mode, QMessageBox.YesRole)
                 menu_mode.addButton(simple_mode, QMessageBox.NoRole)
                 ret = menu_mode.exec_()
-
-                dialog = QFileDialog.getSaveFileName(self, 'Gif', './', 'Gif(*.gif)')
-
-                # notice that press detail_mode will return 0
-                # and simple mode will return 1
-                if not ret:
-                    if dialog:
+                try:
+                    dialog = QFileDialog.getSaveFileName(self, 'Export GIF', './', 'Gif(*.gif)')
+                    # notice that press detail_mode will return 0
+                    # and simple mode will return 1
+                    if dialog and len(dialog[0]) > 0:
                         output_file = dialog[0]
-                        # get each frame
-                        for i, frame in enumerate(main.getFrame(input_file)):
-                            frame.save('gif_frame/frame%d.png' % i, **frame.info)
+                        if not ret:
+                            # get each frame
+                            for i, frame in enumerate(main.getFrameDetail(input_file)):
+                                frame.save('gif_frame/%d.png' % i, **frame.info)
 
-                        # convert all the frame to a .txt file
-                        DIR = 'gif_frame/'
-                        DIR_2 = 'gif_frame_text/'
-                        list_file = os.listdir(DIR)
-                        for i in range(len(list_file)):
-                            main.convertGifDetail(DIR + list_file[i], DIR_2 + 'frame(%d).txt' % i)
+                            # convert all the frame to a .txt file
+                            DIR = 'gif_frame/'
+                            DIR_2 = 'gif_frame_text/'
+                            list_file = natsorted(os.listdir(DIR))
+                            for i in range(len(list_file)):
+                                main.convertGifDetail(DIR + list_file[i], DIR_2 + '%d.txt' % i)
 
-                        # process the .txt files
+                            # process the .txt files
+                            DIR_3 = 'to_image/'
+                            list_file = natsorted(os.listdir(DIR_2))
+                            for i in range(len(list_file)):
+                                main.textToImageDetail(DIR_2 + list_file[i], DIR_3 + '%d.png' % i)
+                        else:
+                            output_file = dialog[0]
+                            # get each frame
+                            for i, frame in enumerate(main.getFrameSimple(input_file)):
+                                frame.save('gif_frame/%d.png' % i, **frame.info)
+
+                            # convert all the frame to a .txt file
+                            DIR = 'gif_frame/'
+                            DIR_2 = 'gif_frame_text/'
+                            list_file = natsorted(os.listdir(DIR))
+                            for i in range(len(list_file)):
+                                main.convertGifSimple(DIR + list_file[i], DIR_2 + '%d.txt' % i)
+
+                            # process the .txt files
+                            DIR_3 = 'to_image/'
+                            list_file = natsorted(os.listdir(DIR_2))
+                            for i in range(len(list_file)):
+                                main.textToImageSimple(DIR_2 + list_file[i], DIR_3 + '%d.png' % i)
+                        # convert back to images
                         DIR_3 = 'to_image/'
-                        list_file = os.listdir(DIR_2)
-                        for i in range(len(list_file)):
-                            main.textToImageDetail(DIR_2 + list_file[i], DIR_3 + 'frame(%d).png' % i)
-                    else:
-                        pass
-                else:
-                    if dialog:
-                        output_file = dialog[0]
-                        # get each frame
-                        for i, frame in enumerate(main.getFrame(input_file)):
-                            frame.save('gif_frame/frame%d.png' % i, **frame.info)
-
-                        # convert all the frame to a .txt file
-                        DIR = 'gif_frame/'
-                        DIR_2 = 'gif_frame_text/'
-                        list_file = os.listdir(DIR)
-                        for i in range(len(list_file)):
-                            main.convertGifSimple(DIR + list_file[i], DIR_2 + 'frame(%d).txt' % i)
-
-                        # process the .txt files
-                        DIR_3 = 'to_image/'
-                        list_file = os.listdir(DIR_2)
-                        for i in range(len(list_file)):
-                            main.textToImageSimple(DIR_2 + list_file[i], DIR_3 + 'frame(%d).png' % i)
-                    else:
-                        pass
-
-                DIR_3 = 'to_image/'
-                # convert all the .process png to .gif
-                if main.imagesToGif(DIR_3, output_file):
-                    msgBox = QMessageBox()
-                    msgBox.setIcon(QMessageBox.Information)
-                    msgBox.setText("Convert successful.")
-                    msgBox.setWindowTitle("Done")
-                    msgBox.exec_()
+                        # convert all the .process png to .gif
+                        if main.imagesToGif(DIR_3, output_file):
+                            msgBox = QMessageBox()
+                            msgBox.setIcon(QMessageBox.Information)
+                            msgBox.setText("Convert successful.")
+                            msgBox.setWindowTitle("Done")
+                            msgBox.exec_()
+                except IOError as io_error:
+                    pass
 
 
 if __name__ == '__main__':
--- TAMU_Hackathon__Pic2ASCII/main.py
+++ TAMU_Hackathon__Pic2ASCII/main.py
@@ -1,14 +1,22 @@
 import os.path
+import sys
+import time
 import numpy as np
 import imageio
+import cv2
+import pygame
 
 from PIL import Image, ImageDraw, ImageFont
 from math import trunc
+from pygame.locals import *
+from natsort import natsorted
 
+
+# convert the image to a txt file
 def convertImage(in_file, out_file):
-    # default size 1000x284
+    # default size 1000 x 300
     base_width = 1000
-    base_size = 284
+    base_size = 300
     # open the image and grey scale it
     img = Image.open(in_file).convert('L')
     # resize the image
@@ -16,7 +24,8 @@ def convertImage(in_file, out_file):
     # get the pixel array
     pixel_Arr = np.array(img)
     # ASCII art
-    ASCII = ['.', ',', '*', '^', '!', '|', '+', '-', ':', ';', 'o', 'x', '%', '?', '$', '#', '@']
+    ASCII = ['!', '|', '$', '%', '*', '+', ',', '-', '.', ':', ';', '?', '@', '^', 'o', 'x', '#']
+
     # assign each character to it corresponding pixel value
     f = open(out_file, 'w')
     for x in pixel_Arr:
@@ -28,7 +37,55 @@ def convertImage(in_file, out_file):
     return True
 
 
-def getFrame(in_file):
+# for convert picture to ASCII picture
+def picToImage(in_file, out_file):
+    # default size 167 x 100
+    base_width = 167
+    base_size = 100
+    # open the image and grey scale it
+    img = Image.open(in_file).convert('L')
+    # resize the image
+    img = img.resize((base_width, base_size), Image.ANTIALIAS)
+    # get the pixel array
+    pixel_Arr = np.array(img)
+    # ASCII art
+    ASCII = ['!', '|', '$', '%', '*', '+', ',', '-', '.', ':', ';', '?', '@', '^', 'o', 'x', '#']
+
+    # create a temp txt file to strore the ASCII char
+    temp = 'temp.txt'
+    # assign each character to it corresponding pixel value
+    f = open(temp, 'w')
+    for x in pixel_Arr:
+        for y in x:
+            f.write(ASCII[trunc(y / 255 * 17) - 1])
+        f.write('\n')
+    # text size 3
+    font = ImageFont.truetype('cour.ttf', 15)
+    # default size 1280 x 1280
+    img = Image.new('L', (1500, 1500), color=255)
+    draw = ImageDraw.Draw(img)
+    vertical_scale = 0
+    with open(temp) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        draw.text((0, vertical_scale), line, fill=0, font=font)
+        vertical_scale = vertical_scale + 15
+    img.save(out_file)
+
+    # close file
+    f.close()
+    os.remove(temp)
+    return True
+
+
+# note for ratio
+# the width of the output gif will be the base_width of the frame * 2
+# the size(height) of the output gif will be the same as the base_size of the frame
+# however be careful with the size of the text and the vertical + horizontal scale
+
+# detail mode
+def getFrameDetail(in_file):
     # get each frame from the gif file
     try:
         i = 0
@@ -40,21 +97,16 @@ def getFrame(in_file):
                 palette = imframe.getpalette()
             else:
                 imframe.putpalette(palette)
-            yield imframe.convert('L')
+            yield imframe.convert('L').resize((500, 500), Image.ANTIALIAS)
             i = i + 1
     except EOFError:
         pass
 
-# note for ratio
-# the width of the output gif will be the base_width of the frame * 2
-# the size(height) of the output gif will be the same as the base_size of the frame
-# however be careful with the size of the text and the vertical + horizontal scale
 
-# detail mode
 def convertGifDetail(in_file, out_file):
-    # default size 256x256
-    base_width = 250
-    base_size = 500
+    # default size 190 x 115
+    base_width = 190
+    base_size = 115
     # open the image and grey scale it
     img = Image.open(in_file).convert('L')
     # resize the image
@@ -62,7 +114,8 @@ def convertGifDetail(in_file, out_file):
     # get the pixel array
     pixel_Arr = np.array(img)
     # ASCII art
-    ASCII = ['.', ',', '*', '^', '!', '|', '+', '-', ':', ';', 'o', 'x', '%', '?', '$', '#', '@']
+    ASCII = ['!', '|', '$', '%', '*', '+', ',', '-', '.', ':', ';', '?', '@', '^', 'o', 'x', '#']
+
     # assign each character to it corresponding pixel value
     f = open(out_file, 'w')
     for x in pixel_Arr:
@@ -75,8 +128,10 @@ def convertGifDetail(in_file, out_file):
 
 
 def textToImageDetail(input_file, output_file):
-    font = ImageFont.truetype('cour.ttf', 3)
-    img = Image.new('L', (500, 500), color=255)
+    # text size 3
+    font = ImageFont.truetype('cour.ttf', 15)
+    # default size 1700 x 1700
+    img = Image.new('L', (1700, 1700), color=255)
     draw = ImageDraw.Draw(img)
     vertical_scale = 0
     with open(input_file) as f:
@@ -84,13 +139,31 @@ def textToImageDetail(input_file, output_file):
 
     for line in lines:
         draw.text((0, vertical_scale), line, fill=0, font=font)
-        vertical_scale = vertical_scale + 1
+        vertical_scale = vertical_scale + 15
     img.save(output_file)
 
 
 # simple mode
+def getFrameSimple(in_file):
+    # get each frame from the gif file
+    try:
+        i = 0
+        img = Image.open(in_file)
+        while 1:
+            img.seek(i)
+            imframe = img.copy()
+            if i == 0:
+                palette = imframe.getpalette()
+            else:
+                imframe.putpalette(palette)
+            yield imframe.convert('L').resize((300, 300), Image.ANTIALIAS)
+            i = i + 1
+    except EOFError:
+        pass
+
+
 def convertGifSimple(in_file, out_file):
-    # default size 100x80
+    # default size 50 x 30
     base_width = 50
     base_size = 30
     # open the image and grey scale it
@@ -100,12 +173,13 @@ def convertGifSimple(in_file, out_file):
     # get the pixel array
     pixel_Arr = np.array(img)
     # ASCII art
-    ASCII = ['.', ',', '*', '^', '!', '|', '+', '-', ':', ';', 'o', 'x', '%', '?', '$', '#', '@']
+    ASCII = ['!', '|', '$', '%', '*', '+', ',', '-', '.', ':', ';', '?', '@', '^', 'o', 'x', '#']
+
     # assign each character to it corresponding pixel value
     f = open(out_file, 'w')
     for x in pixel_Arr:
         for y in x:
-            f.write(ASCII[trunc(y/255*17) - 1])
+            f.write(ASCII[trunc(y/255*8) - 1])
         f.write('\n')
     # close file
     f.close()
@@ -113,8 +187,9 @@ def convertGifSimple(in_file, out_file):
 
 
 def textToImageSimple(input_file, output_file):
+    # text size 10
     font = ImageFont.truetype('cour.ttf', 10)
-    # img = Image.new('L', (800, 800), color=255)
+    # default size 300 x 300
     img = Image.new('L', (300, 300), color=255)
     draw = ImageDraw.Draw(img)
     vertical_scale = 0
@@ -130,7 +205,7 @@ def textToImageSimple(input_file, output_file):
 
 def imagesToGif(input_dir, output_file):
     images = []
-    list_file = os.listdir(input_dir)
+    list_file = natsorted(os.listdir(input_dir))
     for filename in list_file:
         images.append(imageio.imread(input_dir + filename))
     imageio.mimsave(output_file, images)
@@ -148,3 +223,60 @@ def removeFiles():
     list_file = os.listdir('to_image/')
     for file in list_file:
         os.remove('to_image/' + file)
+
+
+def webcamConvert():
+    # for showing the ASCII
+    pygame.init()
+    pygame.font.init()
+    font = pygame.font.SysFont('monospace', 15)
+    size = (1000, 1000)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Output")
+
+    # webcam input
+    name = 'Input'
+    cv2.namedWindow("Input", cv2.WINDOW_AUTOSIZE)
+    vc = cv2.VideoCapture(0)
+
+    cv2.startWindowThread()
+
+    # get the first frame
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    # when we click x on the window, getWindowProperty will returns -1
+    # use this so we could close the window after clicking x
+    while cv2.getWindowProperty(name, 0) >= 0:
+        # gray scale and how the frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        arr = np.array(gray)
+        # show the raw input
+        cv2.imshow(name, gray)
+        rval, frame = vc.read()
+
+        output = ''
+        # put the ASCII on screen
+        text = font.render(output, False, BLACK)
+        screen.fill(WHITE)
+        # quit the pygame when the webcam is closed
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+        screen.blit(text, (0, 0))
+        pygame.display.flip()
+
+        # press escape to quit the webcam
+        key = cv2.waitKey(20)
+        if key == 27:
+            break
+
+    vc.release()
+    cv2.destroyWindow(name)
+
+
+# webcamConvert()
